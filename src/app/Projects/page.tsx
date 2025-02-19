d8a19e81dcde68dd80448001f144bfaed3156310
Rizwanpatel <rizwanpatelmalipatel@gmail.com>
2025-02-20 00:15:38 +0530

    the changes are done

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+"use client";
+
 import { HoverEffect } from "../components/ui/cardHover";
 
-export const projects = [
+export default function Home() {
+  const projects = [
     {
       title: "Stripe",
       description:
@@ -12,42 +15,38 @@ export const projects = [
     {
       title: "Netflix",
       description:
-        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more.",
       link: "https://netflix.com",
     },
     {
       title: "Google",
       description:
-        "A multinational technology company that specializes in Internet-related services and products.",
+        "A multinational technology company specializing in Internet-related services and products.",
       link: "https://google.com",
     },
     {
       title: "Meta",
       description:
-        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+        "A technology company focusing on building products that advance Facebook's mission of bringing the world closer together.",
       link: "https://meta.com",
     },
     {
       title: "Amazon",
       description:
-        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and AI.",
       link: "https://amazon.com",
     },
     {
       title: "Microsoft",
       description:
-        "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+        "A multinational technology company that develops software, consumer electronics, and related services.",
       link: "https://microsoft.com",
     },
   ];
 
-export default function Home() {
   return (
-    <>
-   <div className="max-w-5xl mx-auto px-8">
-      <HoverEffect items={projects}  />
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
     </div>
-    
-    </>  
   );
 }
